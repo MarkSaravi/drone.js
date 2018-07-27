170710d34f88d787db202e2d4ea965e77db144e3
marksaravi <mark.saravi@gmail.com>
2018-07-27 23:52:59 +0000

    refactoring Application

--- src/Application.ts
+++ src/Application.ts
@@ -1,9 +1,29 @@
 ///<reference path="../node_modules/@types/node/index.d.ts" />
 
-import {EventEmitter} from 'events';
-
+import { EventEmitter } from 'events';
+import PortInfo from './models/PortInfo';
 export default class Application extends EventEmitter {
+    devices: PortInfo[] = [];
     constructor() {
         super();
+        this.on('stopping-application', () => {
+        });
+
+        this.on('stop-application', () => {
+            process.exit(0);
+        });
+
+        this.on('devices-ready', (dynamicDevices: PortInfo[], staticDevices: PortInfo[]) => {
+            for (let d of dynamicDevices) {
+                this.devices.push(d);
+            }
+            for (let d of staticDevices) {
+                this.devices.push(d);
+            }
+            for (let d of this.devices) {
+                console.log(`${d.type}: ${d.name}, ${d.baudRate}`);
+            }
+        });
+
     }
 }
--- src/drone.ts
+++ src/drone.ts
@@ -13,26 +13,9 @@ let staticPortsInfo = [
 
 let app = new Application(); 
 
-app.on('devices-ready', (devices: PortInfo[])=>{
-    for (let d of staticPortsInfo) {
-        devices.push(d);
-    }
-    for (let d of devices) {
-        console.log(`${d.type}: ${d.name}, ${d.baudRate}`);
-    }
-    app.on('exit-application',() =>{
-
-    });
-});
-
-app.on('stop-application',()=>{
-    process.exit(0);
-});
-
 let deviceFinder = new DeviceFinder();
 deviceFinder.on('device-detection-completed',(detectedList: PortInfo[]) =>{
-    //console.log('device-detection-completed');
-    app.emit('devices-ready', detectedList);
+    app.emit('devices-ready', detectedList, staticPortsInfo);
 });
 
 deviceFinder.findDevices(dynamicPortsInfo);
