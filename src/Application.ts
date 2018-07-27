a993ccf293d38bc0ba6a948133c850862532b0d6
marksaravi <mark.saravi@gmail.com>
2018-07-27 08:47:28 +0000

    typescript types

--- src/Application.ts
+++ src/Application.ts
@@ -1,4 +1,11 @@
-export default class Application {
+///<reference path="../node_modules/@types/node/index.d.ts" />
+
+import {EventEmitter} from 'events';
+
+export default class Application extends EventEmitter {
+    constructor() {
+        super();
+    }
     greeting(name: String) {
         console.log(`Greeting ${name}`);
     }
