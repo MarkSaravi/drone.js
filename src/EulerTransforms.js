cb9864f1da3d7f631f1120e83f1c699bb35cdc51
marksaravi <mark.saravi@gmail.com>
2018-08-05 02:06:48 +0000

    removing js files

--- src/EulerTransforms.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function RotateAroundZ(roll, pitch, a) {
-    var nroll = Math.cos(a) * roll - Math.sin(a) * pitch;
-    var npitch = Math.sin(a) * roll + Math.cos(a) * pitch;
-    return [nroll, npitch];
-}
-exports.RotateAroundZ = RotateAroundZ;
