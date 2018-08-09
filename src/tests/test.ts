6aa3315ec0037944a80915700a386b3722ed99e3
marksaravi <mark.saravi@gmail.com>
2018-08-09 12:34:47 +0000

    test

--- src/tests/test.ts
+++ src/tests/test.ts
@@ -1,1 +1,29 @@
+import * as flightLogics from '../flight-logics';
+class TestRunner {
+    static currTestTitle: String;
+
+    constructor() {
+        TestRunner.currTestTitle = '';
+    }
+
+    static assert(expected: any, actual: any): void {
+        if (expected === actual) {
+            console.log(`${TestRunner.currTestTitle} is passed`);
+        } else {
+            console.log(`${TestRunner.currTestTitle} is failed`);
+        }
+    }
+
+    test(title: String, testFn:()=>void): void {
+        TestRunner.currTestTitle = title;
+        testFn();
+    }
+}
+
+const runner = new TestRunner();
+
+runner.test('Hellow World', () => {
+    TestRunner.assert(true, true);
+});
+
 console.log('testing...');
