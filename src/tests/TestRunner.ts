ae1b4d963c1468171b964aa79fb1d03834e00640
marksaravi <mark.saravi@gmail.com>
2018-08-09 12:57:46 +0000

    TestRunner

---
+++ src/tests/TestRunner.ts
@@ -0,0 +1,20 @@
+export default class TestRunner {
+    static currTestTitle: String;
+
+    constructor() {
+        TestRunner.currTestTitle = '';
+    }
+
+    static assert(expected: any, actual: any): void {
+        if (expected === actual) {
+            console.log('\x1b[32m', `${TestRunner.currTestTitle} is passed` ,'\x1b[0m');
+        } else {
+            console.log('\x1b[31m', `${TestRunner.currTestTitle} is failed` ,'\x1b[0m');
+        }
+    }
+
+    test(title: String, testFn:()=>void): void {
+        TestRunner.currTestTitle = title;
+        testFn();
+    }
+}
--- src/tests/test.ts
+++ src/tests/test.ts
@@ -1,33 +1,13 @@
+import TestRunner from './TestRunner';
 import * as flightLogics from '../flight-logics';
- 
-class TestRunner {
-    static currTestTitle: String;
-
-    constructor() {
-        TestRunner.currTestTitle = '';
-    }
-
-    static assert(expected: any, actual: any): void {
-        if (expected === actual) {
-            console.log('\x1b[32m', `${TestRunner.currTestTitle} is passed` ,'\x1b[0m');
-        } else {
-            console.log('\x1b[31m', `${TestRunner.currTestTitle} is failed` ,'\x1b[0m');
-        }
-    }
-
-    test(title: String, testFn:()=>void): void {
-        TestRunner.currTestTitle = title;
-        testFn();
-    }
-}
 
 const runner = new TestRunner();
 
-runner.test('Hellow World', () => {
+runner.test('Hellow World passed', () => {
     TestRunner.assert(true, true);
 });
 
-runner.test('Hellow World failing', () => {
+runner.test('Hellow World failed', () => {
     TestRunner.assert(true, false);
 });
 
