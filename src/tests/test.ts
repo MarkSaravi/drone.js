85cf331a1fba7b966a75a65a2c53296258e6e47f
MarkSaravi <mark.saravi.peerjs@gmail.com>
2018-08-10 23:54:54 +1000

    more tests

--- src/tests/test.ts
+++ src/tests/test.ts
@@ -4,7 +4,7 @@ import IPowerCompensations from '../models/IPowerCompensations';
 
 const runner = new TestRunner();
 
-runner.test('Same rpm when zero torque', () => {
+runner.test('zero torque', () => {
     const tr = 0;
     const tp = 0;
     const ty = 0;
@@ -25,7 +25,7 @@ runner.test('Same rpm when zero torque', () => {
     );
 });
 
-runner.test('Same rpm when zero torque', () => {
+runner.test('positive roll torque', () => {
     const tr = 0.1;
     const tp = 0;
     const ty = 0;
@@ -41,7 +41,23 @@ runner.test('Same rpm when zero torque', () => {
     );
 });
 
-runner.test('Same rpm when zero torque', () => {
+runner.test('negative roll torque', () => {
+    const tr = -0.1;
+    const tp = 0;
+    const ty = 0;
+    const pb = 50;
+
+    const actualValues:IPowerCompensations = flightLogics.calcTorques(pb, tr, tp, ty);
+    console.log(`p1: ${actualValues.p1}, p2: ${actualValues.p2}, p3: ${actualValues.p3}, p4: ${actualValues.p4}`);
+    TestRunner.assert(true, 
+        pb === actualValues.p1 &&
+        pb < actualValues.p2 &&
+        pb === actualValues.p3 &&
+        pb > actualValues.p4
+    );
+});
+
+runner.test('positive pitch torque', () => {
     const tr = 0;
     const tp = 0.1;
     const ty = 0;
@@ -56,3 +72,68 @@ runner.test('Same rpm when zero torque', () => {
         pb === Math.round(actualValues.p4)
     );
 });
+
+runner.test('negative pitch torque', () => {
+    const tr = 0;
+    const tp = -0.1;
+    const ty = 0;
+    const pb = 50;
+
+    const actualValues:IPowerCompensations = flightLogics.calcTorques(pb, tr, tp, ty);
+    console.log(`p1: ${actualValues.p1}, p2: ${actualValues.p2}, p3: ${actualValues.p3}, p4: ${actualValues.p4}`);
+    TestRunner.assert(true, 
+        pb < actualValues.p1 &&
+        pb === Math.round(actualValues.p2) &&
+        pb > actualValues.p3 &&
+        pb === Math.round(actualValues.p4)
+    );
+});
+
+runner.test('positive roll and pitch torque', () => {
+    const tr = 0.1;
+    const tp = 0.1;
+    const ty = 0;
+    const pb = 50;
+
+    const actualValues:IPowerCompensations = flightLogics.calcTorques(pb, tr, tp, ty);
+    console.log(`p1: ${actualValues.p1}, p2: ${actualValues.p2}, p3: ${actualValues.p3}, p4: ${actualValues.p4}`);
+    TestRunner.assert(true, 
+        pb > actualValues.p1 &&
+        pb > actualValues.p2 &&
+        pb < actualValues.p3 &&
+        pb < actualValues.p4
+    );
+});
+
+runner.test('negative roll and pitch torque', () => {
+    const tr = -0.1;
+    const tp = -0.1;
+    const ty = 0;
+    const pb = 50;
+
+    const actualValues:IPowerCompensations = flightLogics.calcTorques(pb, tr, tp, ty);
+    console.log(`p1: ${actualValues.p1}, p2: ${actualValues.p2}, p3: ${actualValues.p3}, p4: ${actualValues.p4}`);
+    TestRunner.assert(true, 
+        pb < actualValues.p1 &&
+        pb < actualValues.p2 &&
+        pb > actualValues.p3 &&
+        pb > actualValues.p4
+    );
+});
+
+runner.test('positive roll and negative pitch torque', () => {
+    const tr = -0.1;
+    const tp = -0.1;
+    const ty = 0;
+    const pb = 50;
+
+    const actualValues:IPowerCompensations = flightLogics.calcTorques(pb, tr, tp, ty);
+    console.log(`p1: ${actualValues.p1}, p2: ${actualValues.p2}, p3: ${actualValues.p3}, p4: ${actualValues.p4}`);
+    TestRunner.assert(true, 
+        pb < actualValues.p1 &&
+        pb < actualValues.p2 &&
+        pb > actualValues.p3 &&
+        pb > actualValues.p4
+    );
+});
+
