11726d796c6b990c462ceb57b9d12095328c657a
MarkSaravi <mark.saravi.peerjs@gmail.com>
2018-08-15 17:56:58 +1000

    working nonlinear equation solver

--- calculator.js
+++ calculator.js
@@ -66,6 +66,120 @@ function calcYs(m) {
 
 function calcJx(x1, x2, x3, x4, tr, tp, ty, T) {
     function f1() {
+        return -(x1 * x1 + x2 * x2 + x3 * x3 + x4 * x4 - T);
+    }
+
+    function f2() {
+        return -(x2 * x2 - x4 * x4 + tp);
+    }
+
+    function f3() {
+        return -(x1 * x1 - x3 * x3 + tr);
+    }
+
+    function f4() {
+        return -(x1 * x1 - x2 * x2 + x3 * x3 - x4 * x4 + ty);
+    }
+
+    // x1 * x1 + x2 * x2 + x3 * x3 + x4 * x4 - T  --------------------------------------------------
+    function df1x1() {
+        return 2 * x1;
+    }
+
+    function df1x2() {
+        return 2 * x2;
+    }
+
+    function df1x3() {
+        return 2 * x3;
+    }
+
+    function df1x4() {
+        return 2 * x4;
+    }
+
+    // x2 * x2 - x4 * x4 + tp --------------------------------------------------
+    function df2x1() {
+        return 0;
+    }
+
+    function df2x2() {
+        return 2 * x2;
+    }
+
+    function df2x3() {
+        return 0;
+    }
+
+    function df2x4() {
+        return -2 * x4;
+    }
+
+    // x1 * x1 - x3 * x3 + tr --------------------------------------------------
+    function df3x1() {
+        return 2 * x1;
+    }
+
+    function df3x2() {
+        return 0;
+    }
+
+    function df3x3() {
+        return -2 * x3;
+    }
+
+    function df3x4() {
+        return 0;
+    }
+
+    // x1 * x1 - x2 * x2 + x3 * x3 - x4 * x4 + ty --------------------------------------------------
+    function df4x1() {
+        return 2 * x1;
+    }
+
+    function df4x2() {
+        return -2 * x2;
+    }
+
+    function df4x3() {
+        return 2 * x3;
+    }
+
+    function df4x4() {
+        return -2 * x4;
+    }
+
+    let fx = [f1(), f2(), f3(), f4()];
+    let jx = [
+        [df1x1(), df1x2(), df1x3(), df1x4(), f1()],
+        [df2x1(), df2x2(), df2x3(), df2x4(), f2()],
+        [df3x1(), df3x2(), df3x3(), df3x4(), f3()],
+        [df4x1(), df4x2(), df4x3(), df4x4(), f4()]
+    ];
+    return jx;
+}
+
+const wb = 20;
+const Twb = 4 * wb * wb;
+const Tr = -0.1;
+const Tp = -0.1;
+const Ty = 0;
+let x = [wb, wb, wb, wb];
+
+for (let loop = 0; loop<7; loop++) {
+    let jx = calcJx(x[0],x[1],x[2], x[3], Tr, Tp, Ty, Twb);
+    //console.log(jx);
+    GaussElimination(jx);
+    let y = calcYs(jx);
+    //console.log(y);
+    for (let i=0; i<y.length; i++) {
+        x[i] = x[i] + y[i];
+    }
+    console.log(x);
+}
+
+function calcJx2(x1, x2, x3, x4, tr, tp, ty, T) {
+    function f1() {
         //return x1 * x1 + x2 * x2 + x3 * x3 + x4 * x4 - T;
         return -(3*x1-Math.cos(x2*x3)-1/2);
     }
@@ -178,30 +292,3 @@ function calcJx(x1, x2, x3, x4, tr, tp, ty, T) {
     return jx;
 }
 
-// const wb = 2;
-// const T = 4 * wb * wb;
-// const tr = 2;
-// const tp = 0;
-// const ty = 0;
-// let x = [wb, wb, wb, wb];
-
-//let matrix = [[1,3,-2,5],[3,5,6,7],[2,4,3,8]];
-// let matrix=[[3,2,-4,5,7],[-1,4,1,-2.5,1],[5,1,-1,3,14],[4,3,2,-1,16]];
-// showMatrix(matrix);
-// GaussElimination(matrix);
-// showMatrix(matrix);
-// let y = calcYs(matrix);
-//console.log(showRowMatrix(y, 3));
-let x = [0.1,0.1,-0.1];
-
-for (let loop = 0; loop<7; loop++) {
-    let jx = calcJx(x[0],x[1],x[2]);
-    //console.log(jx);
-    GaussElimination(jx);
-    let y = calcYs(jx);
-    //console.log(y);
-    for (let i=0; i<y.length; i++) {
-        x[i] = x[i] + y[i];
-    }
-    console.log(x);
-}
