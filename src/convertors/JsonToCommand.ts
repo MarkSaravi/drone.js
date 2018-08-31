1375a0da2518a97e04636e4c21be32bd155f2734
MarkSaravi <mark.saravi.peerjs@gmail.com>
2018-09-01 07:04:35 +1000

    bluno beetle and using only power

--- src/convertors/JsonToCommand.ts
+++ src/convertors/JsonToCommand.ts
@@ -2,5 +2,6 @@ import Command from '../models/Command';
 
 export default function(json: string): Command {
     let cmd = JSON.parse(json);
-    return new Command(cmd.h, cmd.x, cmd.y, cmd.p);
+    //return new Command(cmd.h, cmd.x, cmd.y, cmd.p);
+    return new Command(0, 0, 0, cmd.p);
 }
