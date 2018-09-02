e2119ff443066794536b5f66be4c3a9487b5f71a
MarkSaravi <mark.saravi.peerjs@gmail.com>
2018-09-02 13:04:39 +1000

    adding I

--- src/flight-logics/PIDControl.ts
+++ src/flight-logics/PIDControl.ts
@@ -5,9 +5,12 @@ import ITorqueResponse from '../models/ITorqueResponse';
 import IFlightConfig from '../models/IFlightConfig';
 
 export default class PIDControl {
-    prevError: IFlightStateError;
+    integralSumRoll: number = 0;
+    integralSumPitch: number = 0;
+    integralSumYaw: number = 0;
+    prevError: IFlightStateError = null;
+
     constructor() {
-        this.prevError = null;
     }
 
     Pxxx(basePower: number, errors: IFlightStateError, config: any): ICalculatedPowers {
@@ -27,11 +30,17 @@ export default class PIDControl {
     }
 
     I(errors: IFlightStateError, config: IFlightConfig) {
-        if (this.prevError==null) {
+        if (this.prevError == null) {
             this.prevError = errors;
         }
+        const dt = (errors.dt - this.prevError.dt)/1e6;
+        this.integralSumRoll += (this.prevError.rollError + errors.rollError) * dt / 2;
+        this.integralSumPitch += (this.prevError.pitchError + errors.pitchError) * dt / 2;
+        this.integralSumYaw += (this.prevError.yawError + errors.yawError) * dt / 2;
         return {
-            
+            rollTorque: this.integralSumRoll * config.iGain,
+            pitchTorque: this.integralSumPitch * config.iGain,
+            yawTorque: this.integralSumYaw * config.iGain
         }
     }
 
