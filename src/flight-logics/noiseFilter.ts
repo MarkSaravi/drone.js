2df0cb39672300ce2b5a9b85a891fd5bf4d7f970
MarkSaravi <mark.saravi.peerjs@gmail.com>
2018-09-07 04:58:48 +1000

    removing space

--- src/flight-logics/noiseFilter.ts
+++ src/flight-logics/noiseFilter.ts
@@ -2,7 +2,6 @@ const noiseFilter = (value: number, prevFilteredValue: number): number => {
     const w = 0.45;
     const y = w * value + (1 - w) * prevFilteredValue;
     return y;
-
 }
 
 export default noiseFilter;
