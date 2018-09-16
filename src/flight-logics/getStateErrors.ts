c5ecf933f3e058af9a975039c930dd50c422519c
MarkSaravi <mark.saravi.peerjs@gmail.com>
2018-09-17 07:05:17 +1000

    using typed config

--- src/flight-logics/getStateErrors.ts
+++ src/flight-logics/getStateErrors.ts
@@ -1,12 +1,13 @@
 import FlightState from '../models/FlightState';
 import IFlightStateError from '../models/IFlightStateError';
+import IFlightConfig from '../models/IFlightConfig';
 
 const maxAngle = (x: number, max: number) => {
     if (Math.abs(x) <= max) return x;
     return max * Math.sign(x);
 }
 
-export default function getStateError(target: FlightState, actual: FlightState, config: any): IFlightStateError {
+export default function getStateError(target: FlightState, actual: FlightState, config: IFlightConfig): IFlightStateError {
     const rollError = (target.roll - actual.roll);
     const pitchError = (target.pitch - actual.pitch);
     const yawError = (target.yaw - actual.yaw);
