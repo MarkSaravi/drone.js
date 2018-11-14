bb9074ff5e2bea84d6a94b29a0ddf3e78e95dcb2
MarkSaravi <mark.saravi.peerjs@gmail.com>
2018-11-15 08:29:59 +1100

    using command

--- src/application/Application.ts
+++ src/application/Application.ts
@@ -40,6 +40,7 @@ export default class Application extends EventEmitter {
     }
 
     onBleData(bleJson: string) {
+        // {"x":0,"y":-3,"h":0,"p":42.5}
         console.log(bleJson);
         const cmd = convertors.JsonToCommand(bleJson);
         this.flightController.applyCommand(cmd);
