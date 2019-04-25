e10c1a80830ccc24108b1a20721a704803a6ab9d
MarkSaravi <mark.saravi.peerjs@gmail.com>
2019-04-25 10:49:53 +1000

    RemoteControl with low pass filter

--- Arduino/RemoteController/RemoteController.ino
+++ Arduino/RemoteController/RemoteController.ino
@@ -1,75 +1,99 @@
 #include <math.h>
+#include <AltSoftSerial.h>
 
 const float MID_ROLL = 2.5;
 const float MID_PITCH = 2.5;
 const float MID_YAW = 2.4;
+const int STOP = 0;
+const int IDLE = 1;
+const int FLYING = 2;
+const float LOW_PASS_FILTER_FACTOR = 0.45;
 
-float powerVoltage;
-float rollVoltage;
-float pitchVoltage;
-float yawVoltage;
+const int NUM_DATA=4;
+const int POWER_INDEX=0;
+const int ROLL_INDEX=1;
+const int PITCH_INDEX=2;
+const int YAW_INDEX=3;
 
-float prePowerVoltage = 0;
-float preRollVoltage = 0;
-float prePitchVoltage = 0;
-float preYawVoltage = 0;
+float data[NUM_DATA];
+float prevData[NUM_DATA];
+
+char rollstr[16], pitchstr[16], yawstr[16], powerstr[16];
+char jsonstr[256];
 
 long lastSent = 0;
 
-bool isChanged()
+AltSoftSerial BTSerial;
+
+double lowPassFilter(double raw, double filtered)
 {
-  return preRollVoltage != rollVoltage ||
-         prePitchVoltage != pitchVoltage ||
-         preYawVoltage != yawVoltage ||
-         prePowerVoltage != powerVoltage;
+    return raw * LOW_PASS_FILTER_FACTOR + filtered * (1.0 - LOW_PASS_FILTER_FACTOR);
 }
 
-void resetPreVoltage()
+bool isChanged()
 {
-  preRollVoltage = rollVoltage;
-  prePitchVoltage = pitchVoltage;
-  preYawVoltage = yawVoltage;
-  prePowerVoltage = powerVoltage;
+  for (int i=0; i<NUM_DATA; i++) {
+    if (data[i] != prevData[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+void resetPreVoltage(bool setToZero)
+{
+  for (int i=0; i<NUM_DATA; i++) {
+    prevData[i] = setToZero ? 0 : data[i];
+  }
   lastSent = millis();
 }
 
-float sensorValueToVolt(int sensorValue)
+float sensorValueToVolt(float sensorValue)
 {
   return round(sensorValue * 5.0 / 1023.0 * 10) / 10.0;
 }
 
 void readSensorVoltages()
 {
-  rollVoltage = sensorValueToVolt(analogRead(A0)) - MID_ROLL;
-  pitchVoltage = sensorValueToVolt(analogRead(A1)) - MID_PITCH;
-  yawVoltage = sensorValueToVolt(analogRead(A2)) - MID_YAW;
-  powerVoltage = sensorValueToVolt(analogRead(A3));
+  data[ROLL_INDEX] = sensorValueToVolt(analogRead(A0) - MID_ROLL);
+  data[PITCH_INDEX] = sensorValueToVolt(analogRead(A1) - MID_PITCH);
+  data[YAW_INDEX] = sensorValueToVolt(analogRead(A2) - MID_YAW);
+  data[POWER_INDEX] = sensorValueToVolt(analogRead(A3));
+}
+
+void applyLowPassFilter() {
+  for (int i=0; i<NUM_DATA; i++) {
+    data[i] = lowPassFilter(data[i], prevData[i]);
+  }
 }
 
 void sendJsonData()
 {
-  Serial.print("{\"roll\":");
-  Serial.print(rollVoltage);
-  Serial.print(",\"pitch\":");
-  Serial.print(pitchVoltage);
-  Serial.print(",\"yaw\":");
-  Serial.print(yawVoltage);
-  Serial.print(",\"power\":");
-  Serial.print(powerVoltage);
-  Serial.println("}");
+  dtostrf(data[ROLL_INDEX], 0, 1, rollstr);
+  dtostrf(data[PITCH_INDEX], 0, 1, pitchstr);
+  dtostrf(data[YAW_INDEX], 0, 1, yawstr);
+  dtostrf(data[POWER_INDEX], 0, 1, powerstr);
+  sprintf(jsonstr, 
+    "{\"state\":%d,\"roll\":%s,\"pitch\":%s,\"yaw\":%s,\"power\":%s,\"time\":%d}\n", 
+    FLYING, rollstr, pitchstr, yawstr, powerstr, lastSent);
+  Serial.print(jsonstr);
+  BTSerial.write(jsonstr);
 }
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(57600);
+  BTSerial.begin(57600);
+  resetPreVoltage(true);
 }
 
 void loop()
 {
   readSensorVoltages();
-  if (isChanged() || (millis() - lastSent >= 200))
+  if (isChanged() || (millis() - lastSent >= 250))
   {
     sendJsonData();
-    resetPreVoltage();
+    resetPreVoltage(false);
   }
+  delay(50);
 }
