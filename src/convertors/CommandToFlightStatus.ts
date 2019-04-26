517fe0bac390c6a5b6acc016f3698d1656613dda
MarkSaravi <mark.saravi.peerjs@gmail.com>
2019-04-26 23:33:28 +1000

    removing old command conversions

--- src/convertors/CommandToFlightStatus.ts
+++
@@ -1,7 +0,0 @@
-import FlightState from '../models/FlightState';
-import Command from '../models/Command';
-
-function CommandToFlightStatus(command: Command): FlightState {
-    return new FlightState(command.x, command.y, command.heading, 0, command.power);
-}
-export default CommandToFlightStatus;
--- src/convertors/index.ts
+++ src/convertors/index.ts
@@ -1,9 +1,5 @@
 import ImuDataToFlightStatus from './ImuDataToFlightStatus';
-import CommandToFlightStatus from './CommandToFlightStatus';
 import JsonToImuData from './JsonToImuData';
-import JsonToCommand from './JsonToCommand';
 
-export {CommandToFlightStatus};
 export {ImuDataToFlightStatus};
-export {JsonToCommand};
 export {JsonToImuData};
